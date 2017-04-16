a852f11be3dece304182e4809491ffff2eb17605
Rob Crocombe <rjwc1993@yahoo.co.uk>
2017-04-16 23:05:35 +0100

    Added basic app and moved to gulp and webpack

---
+++ gulpfile.babel.js
@@ -0,0 +1,80 @@
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import path from 'path';
+import webpack from 'webpack';
+
+let firstRun = true;
+
+const jsConfig = {
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  entry: {
+    app: path.resolve(__dirname, 'src/app.js')
+  },
+  output: {
+    path: path.resolve(__dirname,'build/'),
+    filename: 'app.bundle.js'
+  }
+};
+
+function runWebpack(config, done) {
+  webpack(config, (err, stats) => {
+    if (err) {
+      gulp.emit('error', new gutil.PluginError('webpack', err));
+    }
+
+    gutil.log(
+      '[webpack]',
+      stats.toString({
+        colors: gutil.colors.supportsColor,
+        hash: false,
+        timings: false,
+        chunks: false,
+        chunkModules: false,
+        modules: false,
+        children: true,
+        version: true,
+        cached: false,
+        cachedAssets: false,
+        reasons: false,
+        source: false,
+        errorDetails: false,
+      }),
+    );
+
+    if (firstRun) {
+      firstRun = false;
+      done();
+    }
+  });
+}
+
+function jsDev(done) {
+  const config = Object.assign({
+    watch: true,
+  }, jsConfig);
+
+  runWebpack(config, done);
+}
+
+function jsProd(done) {
+  const config = Object.assign({
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin(),
+    ],
+  }, jsConfig);
+
+  runWebpack(config, done);
+}
+
+gulp.task('dev:build', jsDev);
+gulp.task('pro:build', jsProd);
+gulp.task('default', jsDev);
---
+++ src/app.js
@@ -0,0 +1,10 @@
+class App {
+  constructor() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+  }
+
+  draw() {
+
+  }
+}
